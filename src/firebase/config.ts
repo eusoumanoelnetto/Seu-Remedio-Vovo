3b0fd0dc92445eef2ee4a6de2c26300b36d55717
Manoel Coelho <manoelccoelho@gmail.com>
2026-03-12 21:36:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6628206611-fb17f",
+  "appId": "1:1012363052381:web:1c2fb589450ee6ef6454b4",
+  "apiKey": "AIzaSyCdy9r_WnLwix7YJq9wbHtUDG4SdCIVRVE",
+  "authDomain": "studio-6628206611-fb17f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1012363052381"
+};
